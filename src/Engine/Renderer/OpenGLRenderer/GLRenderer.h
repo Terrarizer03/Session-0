ef6264961d41538acb609623a26942967b75fa92
Terrarizer <terrarizer0399@gmail.com>
2026-01-21 23:18:20 +0800

    added renderer abstraction

---
+++ src/Engine/Renderer/OpenGLRenderer/GLRenderer.h
@@ -0,0 +1,13 @@
+//
+// Created by Terrarizer on 21/01/2026.
+//
+
+#ifndef DNDCREATOR_GLRENDERER_H
+#define DNDCREATOR_GLRENDERER_H
+
+
+class GLRenderer {
+};
+
+
+#endif //DNDCREATOR_GLRENDERER_H
